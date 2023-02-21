e770eb7de51f7d8d84411c8f2c8b80f16076cbb3
BIBruington <psychological_chemist@hotmail.com>
2023-02-21 00:16:24 -0500

    banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100 ">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
+
       {/* Header component */}
 
       <Header />
+
+      {/* Main area */}
+
+      <main className="max-w-screen-2xl mx-auto">
+
+        {/* Banner */}
+
+        <Banner />
+
+        {/* ProductFeed */}
+
+      </main>
+
     </div>
   );
 }

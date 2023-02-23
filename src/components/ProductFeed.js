09e409253345bcbf5abd43755ed01f08e6fe8ab4
BIBruington <psychological_chemist@hotmail.com>
2023-02-22 20:41:44 -0500

    product feed cleanup

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,9 +3,12 @@ import Product from "./Product";
 export default function ProductFeed({products}) {
 
   return (
+    // grid-flow-row-dense says to make sure to use up empty space for the row
     <div className="grid grid-flow-row-dense md:grid-cols-2 
-    lg:grid-cols-3 xl:grid-cols-4 md:-mt-44 mx-auto">
-      {products && products.map(({id, title, price, description, category, image}) => (
+      lg:grid-cols-3 xl:grid-cols-4 md:-mt-44 mx-auto">
+      {products && products
+      .slice(0,4)
+      .map(({id, title, price, description, category, image}) => (
         <Product 
           key={id} 
           id={id}
@@ -16,6 +19,43 @@ export default function ProductFeed({products}) {
           image={image}
         />
       ))}
+
+      <img 
+        className="md:col-span-full" 
+        src="https://links.papareact.com/dyz" 
+        alt="" 
+      />
+
+      <div className="md:col-span-2">
+        {products && products
+        .slice(4,5)
+        .map(({id, title, price, description, category, image}) => (
+          <Product 
+          key={id} 
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+          />
+        ))}
+      </div>
+
+      {products && products
+        .slice(5, products.length)
+        .map(({id, title, price, description, category, image}) => (
+          <Product 
+          key={id} 
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+          />
+        ))}
+
     </div>
   )
 }
